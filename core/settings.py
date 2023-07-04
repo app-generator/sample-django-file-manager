5b6068c3318f983667c937d9c9c30b0174e19cbf
mominur-helios <mominur.rahman@heliosinsider.com>
2023-07-04 16:13:07 +0600

    make view options modal popup

--- core/settings.py
+++ core/settings.py
@@ -171,3 +171,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
